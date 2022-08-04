e781a6bc49fb6b9554fe38ff28ea5d91bae453f4
Sergio Berdeja <sergioberdeja@gmail.com>
2022-08-04 14:39:38 -0600

    Completed assignment

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = URI.open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+puts doc.css(".footer-address").text.strip
+
+footers = doc.css(".footer-list.list-stacked")
+footers.each do |item|
+    puts item.text.strip
+end
